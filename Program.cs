70defbf482021d0983e01867dc8faadd60f83916
0x-2FA <2fa_gh@protonmail.com>
2023-02-07 15:10:38 +0200

    Added the connection string and options for auth

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using smart_alert_api.Models.Database;
+using smart_alert_api.Services.Auth;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SmartAlertContextConnection") ?? throw new InvalidOperationException("Connection string 'SmartAlertContextConnection' not found.");
+
+builder.Services.AddDbContext<SmartAlertContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddDefaultIdentity<EntityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 8;
+    })
+    .AddEntityFrameworkStores<SmartAlertContext>();
 
 builder.Services.AddControllers();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
