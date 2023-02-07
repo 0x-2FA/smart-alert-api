6c2304d0e82e25bf2a4be46f6dd27f4d4c37f103
0x-2FA <2fa_gh@protonmail.com>
2023-02-07 15:17:37 +0200

    Added the Auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using smart_alert_api.Models.Auth;
+using smart_alert_api.Services.Auth;
+
+namespace smart_alert_api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest registerRequest)
+        {
+            var authResult = _authService.Register(
+                registerRequest.email, 
+                registerRequest.phone, 
+                registerRequest.password);
+
+            var response = new AuthResponse(authResult.email);
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            var authResult = _authService.Login(
+                loginRequest.email,
+                loginRequest.password);
+
+            var response = new AuthResponse(authResult.email);
+
+            return Ok(response);
+        }
+    }
+}
