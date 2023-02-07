6f5ffa427e3f0652ac79c7a3250265de68d8b41a
0x-2FA <2fa_gh@protonmail.com>
2023-02-07 15:13:16 +0200

    Added db context and identity user

---
+++ Models/Database/EntityUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace smart_alert_api.Models.Database;
+
+public class EntityUser : IdentityUser
+{
+}
+
