73f94053e9af6012e6765bb985d1330f45bf5984
0x-2FA <2fa_gh@protonmail.com>
2023-02-08 17:17:25 +0200

    Added checks in Login and Register actions

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using smart_alert_api.Interfaces;
 using smart_alert_api.Models.Auth;
 using smart_alert_api.Services.Auth;
 
@@ -9,21 +10,28 @@ namespace smart_alert_api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly IUserRepository _userRepository;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService, IUserRepository userRepository)
         {
             _authService = authService;
+            _userRepository = userRepository;
         }
 
         [HttpPost("register")]
         public IActionResult Register(RegisterRequest registerRequest)
         {
+            if (_userRepository.FindUserByEmail(registerRequest.email) != null)
+            {
+                return BadRequest();
+            }
+
             var authResult = _authService.Register(
                 registerRequest.email, 
                 registerRequest.phone, 
                 registerRequest.password);
 
-            var response = new AuthResponse(authResult.email);
+            var response = new AuthResponse(authResult.user.Email);
 
             return Ok(response);
         }
@@ -31,11 +39,22 @@ namespace smart_alert_api.Controllers
         [HttpPost("login")]
         public IActionResult Login(LoginRequest loginRequest)
         {
+ 
+            if (_userRepository.FindUserByEmail(loginRequest.email) == null)
+            {
+                return BadRequest();
+            }
+
+            if (!_userRepository.IsPasswordValid(loginRequest.email, loginRequest.password))
+            {
+                return BadRequest();
+            }
+
             var authResult = _authService.Login(
                 loginRequest.email,
                 loginRequest.password);
 
-            var response = new AuthResponse(authResult.email);
+            var response = new AuthResponse(authResult.user.Email);
 
             return Ok(response);
         }
