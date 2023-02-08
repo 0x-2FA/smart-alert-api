39cd3c16992eed80caa272be8a0b64668c106bbe
0x-2FA <2fa_gh@protonmail.com>
2023-02-08 17:05:00 +0200

    Added the User utilities implementation

---
+++ Utilities/EntityUserUtilities.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using smart_alert_api.Interfaces;
+using smart_alert_api.Models.Database;
+
+
+namespace smart_alert_api.Utilities
+{
+    public class EntityUserUtilities : IEntityUserUtilities
+    {
+        private readonly SignInManager<EntityUser> _signInManager;
+        private readonly IUserStore<EntityUser> _userStore;
+
+        public EntityUserUtilities(SignInManager<EntityUser> signInManager, IUserStore<EntityUser> userStore)
+        {
+            _signInManager = signInManager;
+            _userStore = userStore;
+        }
+
+        public EntityUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<EntityUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException("Can't create instance of Entity User");
+            }
+        }
+
+        public IUserEmailStore<EntityUser> GetEmailStore()
+        {
+            if(!_signInManager.UserManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("Requires a user store with email support.");
+            }
+
+            return (IUserEmailStore<EntityUser>)_userStore;
+        }
+    }
+}
