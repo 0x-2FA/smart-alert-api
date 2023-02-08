52fc7910c806ccbd03fcffd19374d3756330b2c5
0x-2FA <2fa_gh@protonmail.com>
2023-02-08 15:54:17 +0200

    Added the User utilities interface

---
+++ Interfaces/IEntityUserUtilities.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Interfaces
+{
+    public interface IEntityUserUtilities
+    {
+        EntityUser CreateUser();
+
+        IUserEmailStore<EntityUser> GetEmailStore();
+    }
+}
