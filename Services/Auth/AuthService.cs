eb4c92c7101056fee141fd627789f9ed7d6d606a
0x-2FA <2fa_gh@protonmail.com>
2023-02-08 17:11:56 +0200

    Implemented proper Login and Register service

--- Services/Auth/AuthService.cs
+++ Services/Auth/AuthService.cs
@@ -1,15 +1,34 @@
-﻿namespace smart_alert_api.Services.Auth
+﻿using smart_alert_api.Interfaces;
+
+namespace smart_alert_api.Services.Auth
 {
     public class AuthService : IAuthService
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IEntityUserUtilities _entityUserUtilities;
+
+        public AuthService(IUserRepository userRepository, IEntityUserUtilities entityUserUtilities)
+        {
+            _userRepository = userRepository;
+            _entityUserUtilities = entityUserUtilities;
+        }
+
         public AuthResult Login(string email, string password)
         {
-            return new AuthResult(email);
+            var user = _userRepository.FindUserByEmail(email);
+
+            return new AuthResult(user);
         }
 
         public AuthResult Register(string email, string phone, string password)
         {
-            return new AuthResult(email);
+            var user = _entityUserUtilities.CreateUser();
+            user.Email = email;
+            user.PhoneNumber = phone;
+
+            _userRepository.AddUser(user, password);
+
+            return new AuthResult(user);
         }
     }
 }
