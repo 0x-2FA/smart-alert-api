1c875256b31ddf9967b9cfa578a0429c7a2849ac
0x-2FA <2fa_gh@protonmail.com>
2023-02-08 15:53:38 +0200

    Added the User repository interface

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Interfaces
+{
+    public interface IUserRepository
+    {
+        EntityUser? FindUserByEmail(string email);
+
+        Boolean IsPasswordValid(string email, string password);
+
+        void AddUser(EntityUser user, string password);
+    }
+}
