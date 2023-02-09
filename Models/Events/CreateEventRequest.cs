91f70d2275c8305b7f5c7c4a1a8c8ea791b08b57
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 22:35:23 +0200

    Added the lat and long fields

--- Models/Events/CreateEventRequest.cs
+++ Models/Events/CreateEventRequest.cs
@@ -2,5 +2,5 @@
 
 namespace smart_alert_api.Models.Events
 {
-    public record CreateEventRequest(string type, string? photo, string? comment, string user_id);
+    public record CreateEventRequest(string type, string latitude, string longitude, string? photo, string? comment, string user_id);
 }
