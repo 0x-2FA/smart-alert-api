0069b56a8ce8b2b785495d99315ab8fa3b095b31
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 15:18:44 +0200

    Implemented the event repository interface

---
+++ Repositories/EventRepository.cs
@@ -0,0 +1,37 @@
+﻿using smart_alert_api.Interfaces;
+using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly SmartAlertContext _context;
+
+        public EventRepository(SmartAlertContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateEvent(Event evnt)
+        {
+            _context.Events.AddAsync(evnt);
+
+            _context.SaveChangesAsync();
+        }
+
+        public void DeleteEventById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event? FindEventById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event>? FindEventsByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
