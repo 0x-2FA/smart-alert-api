96f0eba33bb022ce936078320941b037187b51cf
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 15:18:17 +0200

    Created the event repository interface

---
+++ Interfaces/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Interfaces
+{
+    public interface IEventRepository
+    {
+        Event? FindEventById(long id);
+        List<Event>? FindEventsByUserId(string userId);
+        void CreateEvent(Event evnt);
+        void DeleteEventById(long id);
+    }
+}
