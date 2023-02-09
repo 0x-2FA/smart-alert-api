ff3cc1c0e2204d4521fbbf781764fdbb780b0f14
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 12:48:24 +0200

    Created the Events table

--- Models/Database/SmartAlertContext.cs
+++ Models/Database/SmartAlertContext.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace smart_alert_api.Models.Database;
-
-public class SmartAlertContext : IdentityDbContext<EntityUser>
+namespace smart_alert_api.Models.Database
 {
-    public SmartAlertContext(DbContextOptions<SmartAlertContext> options)
-        : base(options)
+    public partial class SmartAlertContext : IdentityDbContext<EntityUser>
     {
-    }
+        public SmartAlertContext()
+        {
+        }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
+        public SmartAlertContext(DbContextOptions<SmartAlertContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Event> Events { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.HasIndex(e => e.UserId, "IX_Events_user_id");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Comment).HasColumnName("comment");
+
+                entity.Property(e => e.Photo).HasColumnName("photo");
+
+                entity.Property(e => e.Timestamp).HasColumnName("timestamp");
+
+                entity.Property(e => e.Type).HasColumnName("type");
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+                
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
