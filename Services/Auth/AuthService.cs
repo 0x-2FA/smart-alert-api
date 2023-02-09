8375a1dc5a55375c9949704bcfb1d8ada73dd744
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 22:45:09 +0200

    Implemented the logout method and on login actually sign in the user

--- Services/Auth/AuthService.cs
+++ Services/Auth/AuthService.cs
@@ -1,4 +1,6 @@
-﻿using smart_alert_api.Interfaces;
+﻿using Microsoft.AspNetCore.Identity;
+using smart_alert_api.Interfaces;
+using smart_alert_api.Models.Database;
 
 namespace smart_alert_api.Services.Auth
 {
@@ -6,18 +8,28 @@ namespace smart_alert_api.Services.Auth
     {
         private readonly IUserRepository _userRepository;
         private readonly IEntityUserUtilities _entityUserUtilities;
+        private readonly SignInManager<EntityUser> _signInManager;
 
-        public AuthService(IUserRepository userRepository, IEntityUserUtilities entityUserUtilities)
+
+        public AuthService(IUserRepository userRepository, IEntityUserUtilities entityUserUtilities, SignInManager<EntityUser> signInManager)
         {
             _userRepository = userRepository;
             _entityUserUtilities = entityUserUtilities;
+            _signInManager = signInManager;
         }
 
         public AuthResult Login(string email, string password)
         {
             var user = _userRepository.FindUserByEmail(email);
 
+            _signInManager.PasswordSignInAsync(user, password, isPersistent: false, lockoutOnFailure: false); ;
+
             return new AuthResult(user);
+        }
+
+        public void Logout()
+        {
+            _signInManager.SignOutAsync();
         }
 
         public AuthResult Register(string email, string phone, string password)
@@ -26,7 +38,7 @@ namespace smart_alert_api.Services.Auth
             user.Email = email;
             user.PhoneNumber = phone;
 
-            _userRepository.AddUser(user, password);
+            _userRepository.AddUser(user, password, "Civilian");
 
             return new AuthResult(user);
         }
