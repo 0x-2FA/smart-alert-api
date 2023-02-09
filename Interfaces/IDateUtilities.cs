1a2f5fdd8e75ba5af18fe0d86dcd3af70b72d2d1
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 15:07:01 +0200

    Added interface for date and time utilities

---
+++ Interfaces/IDateUtilities.cs
@@ -0,0 +1,10 @@
+﻿namespace smart_alert_api.Interfaces
+{
+    public interface IDateUtilities
+    {
+        public string NowDefaultString();
+
+        public string NowCustomString(string format);
+
+    }
+}
