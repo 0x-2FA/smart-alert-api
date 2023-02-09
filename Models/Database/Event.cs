6f68963ac8135dab61bcb0410a9c8dd4adb4f79c
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 22:29:26 +0200

    Added latitude and longitude fields

--- Models/Database/Event.cs
+++ Models/Database/Event.cs
@@ -8,6 +8,8 @@ namespace smart_alert_api.Models.Database
         public long Id { get; set; }
         public string? Type { get; set; }
         public string? Timestamp { get; set; }
+        public string? Latitude { get; set; }
+        public string? Longitude { get; set; }
         public string? Photo { get; set; }
         public string? Comment { get; set; }
         public string? UserId { get; set; }
