c4e045dc59838b9cf86e317eba82b6b8c688b38f
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 22:57:01 +0200

    Added the logout action

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using smart_alert_api.Interfaces;
 using smart_alert_api.Models.Auth;
+using smart_alert_api.Models.Database;
 using smart_alert_api.Services.Auth;
 
 namespace smart_alert_api.Controllers
@@ -11,11 +13,14 @@ namespace smart_alert_api.Controllers
     {
         private readonly IAuthService _authService;
         private readonly IUserRepository _userRepository;
+        private readonly SignInManager<EntityUser> _signInManager;
 
-        public AuthController(IAuthService authService, IUserRepository userRepository)
+
+        public AuthController(IAuthService authService, IUserRepository userRepository, SignInManager<EntityUser> signInManager)
         {
             _authService = authService;
             _userRepository = userRepository;
+            _signInManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -39,7 +44,6 @@ namespace smart_alert_api.Controllers
         [HttpPost("login")]
         public IActionResult Login(LoginRequest loginRequest)
         {
- 
             if (_userRepository.FindUserByEmail(loginRequest.email) == null)
             {
                 return BadRequest();
@@ -57,6 +61,24 @@ namespace smart_alert_api.Controllers
             var response = new AuthResponse(authResult.user.Email);
 
             return Ok(response);
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout(LogoutRequest logoutRequest)
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return Unauthorized();
+            }
+
+            if (_userRepository.FindUserByEmail(logoutRequest.email) == null)
+            {
+                return BadRequest();
+            }
+
+            _authService.Logout();
+
+            return Ok();
         }
     }
 }
