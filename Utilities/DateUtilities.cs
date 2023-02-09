f11e406eea08b717a5d6bfcbd288410935fa8fd9
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 15:07:29 +0200

    Implemented the date and time utilities interface

---
+++ Utilities/DateUtilities.cs
@@ -0,0 +1,23 @@
+﻿using smart_alert_api.Interfaces;
+
+namespace smart_alert_api.Utilities
+{
+    public class DateUtilities : IDateUtilities
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+
+        public string NowDefaultString()
+        {
+            string format = "dd/MM/yyyy HH:mm";
+            return DateTime.Now.ToString(format);
+        }
+
+        public string NowCustomString(string format)
+        {
+            return DateTime.Now.ToString(format);
+        }
+    }
+}
