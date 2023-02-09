4a522562c91501ea971e5bc195c03ed555acd38f
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 22:58:01 +0200

    Added Get and Delete actions

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using smart_alert_api.Interfaces;
 using smart_alert_api.Models.Database;
@@ -13,20 +15,63 @@ namespace smart_alert_api.Controllers
     {
         private readonly IEventService _eventService;
         private readonly IEventRepository _eventRepository;
+        private readonly SignInManager<EntityUser> _signInManager;
 
-        public EventsController(IEventService eventService, IEventRepository eventRepository)
+        public EventsController(IEventService eventService, IEventRepository eventRepository, SignInManager<EntityUser> signInManager)
         {
             _eventService = eventService;
             _eventRepository = eventRepository;
+            _signInManager = signInManager;
         }
 
+        [HttpGet("{id:long}")]
+        public IActionResult Get(long id)
+        {
+            if (_eventRepository.FindEventById(id) == null)
+            {
+                return BadRequest();
+            }
+
+            var eventResult = _eventService.GetEvent(id);
+
+            var response = new EventsResponse(eventResult.evnt);
+
+            return Ok(response.evnt);
+        }
+
+        [HttpDelete("{id:long}")]
+        public IActionResult Delete(long id)
+        {
+            if(!_signInManager.IsSignedIn(User))
+            {
+                return Unauthorized();
+            }
+
+            if(!User.IsInRole("CivilProtection"))
+            {
+                return StatusCode(403);
+            }
+
+            if (_eventRepository.FindEventById(id) == null)
+            {
+                return BadRequest(id);
+            }
+
+            var eventResult = _eventService.DeleteEvent(id);
+
+            var response = new EventDeletedResponse(eventResult.id);
+
+            return Ok(response);
+        }
 
         [HttpPost("create")]
         public IActionResult Create(CreateEventRequest createRequest)
         {
-            
+
             var eventResult = _eventService.CreateEvent(
                 createRequest.type,
+                createRequest.latitude,
+                createRequest.longitude,
                 createRequest.photo,
                 createRequest.comment,
                 createRequest.user_id);
