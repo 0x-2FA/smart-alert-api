0307608a66084b591ed7a8b7a6d2fdbc26b7866d
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 15:21:31 +0200

    Created the events controller

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using smart_alert_api.Interfaces;
+using smart_alert_api.Models.Database;
+using smart_alert_api.Models.Events;
+using smart_alert_api.Services.Events;
+
+namespace smart_alert_api.Controllers
+{
+    [ApiController]
+    [Route("events")]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+        private readonly IEventRepository _eventRepository;
+
+        public EventsController(IEventService eventService, IEventRepository eventRepository)
+        {
+            _eventService = eventService;
+            _eventRepository = eventRepository;
+        }
+
+
+        [HttpPost("create")]
+        public IActionResult Create(CreateEventRequest createRequest)
+        {
+            
+            var eventResult = _eventService.CreateEvent(
+                createRequest.type,
+                createRequest.photo,
+                createRequest.comment,
+                createRequest.user_id);
+
+            var response = new EventsResponse(eventResult.evnt);
+
+            return Created(nameof(response.evnt), response.evnt);
+        }
+    }
+}
