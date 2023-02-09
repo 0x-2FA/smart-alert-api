9d77bc3665e3bbaf300393012f43359bb4cb267f
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 15:20:20 +0200

    Added some records

---
+++ Models/Events/CreateEventRequest.cs
@@ -0,0 +1,6 @@
+﻿using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Models.Events
+{
+    public record CreateEventRequest(string type, string? photo, string? comment, string user_id);
+}
