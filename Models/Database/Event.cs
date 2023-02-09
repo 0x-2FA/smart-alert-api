af8dd2c992b57a662a451de9a7f6407d3e971a40
0x-2FA <2fa_gh@protonmail.com>
2023-02-09 12:44:37 +0200

    Added the Event model

---
+++ Models/Database/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace smart_alert_api.Models.Database
+{
+    public partial class Event
+    {
+        public long Id { get; set; }
+        public string? Type { get; set; }
+        public string? Timestamp { get; set; }
+        public string? Photo { get; set; }
+        public string? Comment { get; set; }
+        public string? UserId { get; set; }
+
+        public virtual EntityUser? User { get; set; }
+    }
+}
