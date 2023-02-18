566a3a593bbb675298c6a87814b13e5f7862111e
0x-2FA <2fa_gh@protonmail.com>
2023-02-19 01:46:59 +0200

    Implemented the IGeoLocation interface

---
+++ Utilities/GeoLocation.cs
@@ -0,0 +1,14 @@
+﻿using Geolocation;
+using smart_alert_api.Interfaces;
+
+namespace smart_alert_api.Utilities
+{
+    public class GeoLocation : IGeoLocation
+    {
+        public double GetDistanceInKm(double latitude, double longitude, double otherLatitude, double otherLongitude)
+        {
+            var mileToKm = 1.609344;
+            return GeoCalculator.GetDistance(latitude, longitude, otherLatitude, otherLongitude) * mileToKm;
+        }
+    }
+}
