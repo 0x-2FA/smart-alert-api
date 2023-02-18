e36a10e00cfd9cd11ae215909cf1107669b9c188
0x-2FA <2fa_gh@protonmail.com>
2023-02-18 13:05:14 +0200

    Implemented the newest utility methods

--- Utilities/DateUtilities.cs
+++ Utilities/DateUtilities.cs
@@ -19,5 +19,33 @@ namespace smart_alert_api.Utilities
         {
             return DateTime.Now.ToString(format);
         }
+
+        public string GetDateOnlyString(string timestamp)
+        {
+            DateTime timestampDateTime = DateTime.Parse(timestamp);
+
+            DateOnly dateOnly = DateOnly.FromDateTime(timestampDateTime);
+
+            return dateOnly.ToString();
+        }
+
+        public string DatePreviousDayString(string date)
+        {
+            DateTime dateTime = DateTime.Parse(date);
+
+            DateTime previousDateTime = dateTime.AddDays(-1);
+
+            return GetDateOnlyString(previousDateTime.ToString());
+        }
+
+        public string DatePreviousDayCustomString(string date, string format)
+        {
+            DateTime dateTime = DateTime.Parse(date);
+
+            DateTime previousDateTime = dateTime.AddDays(-1);
+
+            return previousDateTime.ToString(format);
+        }
+
     }
 }
