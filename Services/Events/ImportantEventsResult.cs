ec6fb184e60276793105d9b429d6b007fb612d06
0x-2FA <2fa_gh@protonmail.com>
2023-02-19 01:50:46 +0200

    Created new result for important events

---
+++ Services/Events/ImportantEventsResult.cs
@@ -0,0 +1,6 @@
+﻿using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Services.Events
+{
+    public record ImportantEventsResult(List<Event>? earthquakeEvents, List<Event>? fireEvents, List<Event>? floodEvents);
+}
