f79a36450fa4607b837ab9d171b4cd00b6592f57
0x-2FA <2fa_gh@protonmail.com>
2023-02-19 01:46:09 +0200

    Created the GeoLocation interface

---
+++ Interfaces/IGeoLocation.cs
@@ -0,0 +1,7 @@
+﻿namespace smart_alert_api.Interfaces
+{
+    public interface IGeoLocation
+    {
+        public double GetDistanceInKm(double latitude, double longitude, double otherLatitude, double otherLongitude);
+    }
+}
