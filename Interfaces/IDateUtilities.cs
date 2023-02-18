a3a135e12a7ab3d81cba09a4a752e4dd49183803
0x-2FA <2fa_gh@protonmail.com>
2023-02-18 13:04:48 +0200

    Added more date utility methods

--- Interfaces/IDateUtilities.cs
+++ Interfaces/IDateUtilities.cs
@@ -6,5 +6,11 @@
 
         public string NowCustomString(string format);
 
+        public string GetDateOnlyString(string timestamp);
+
+        public string DatePreviousDayString(string date);
+
+        public string DatePreviousDayCustomString(string date, string format);
+
     }
 }
