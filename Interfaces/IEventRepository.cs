d4d6fcec6b62225358e145304e8534f0d5f35e22
0x-2FA <2fa_gh@protonmail.com>
2023-02-18 13:10:04 +0200

    Added method from get all the events by specific type

--- Interfaces/IEventRepository.cs
+++ Interfaces/IEventRepository.cs
@@ -7,6 +7,7 @@ namespace smart_alert_api.Interfaces
         Event? FindEventById(long id);
         List<Event>? FindEventsByUserId(string userId);
         List<Event>? GetAllEvents();
+        List<Event> GetEventsByType(string type);
         void CreateEvent(Event evnt);
         void DeleteEventById(long id);
     }
