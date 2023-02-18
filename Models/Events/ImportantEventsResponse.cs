fd002a26e658b7c288bed3d62931f18e1e09f6eb
0x-2FA <2fa_gh@protonmail.com>
2023-02-19 01:51:26 +0200

    Added model for important events response

---
+++ Models/Events/ImportantEventsResponse.cs
@@ -0,0 +1,6 @@
+﻿using smart_alert_api.Models.Database;
+
+namespace smart_alert_api.Models.Events
+{
+    public record ImportantEventsResponse(List<Event>? listEarthquake, List<Event>? fireEvents, List<Event>? floodEvents);
+}
