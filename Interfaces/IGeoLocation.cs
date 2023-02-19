2bfe3b69b2974e4887715f699fd78871db146fc8
0x-2FA <2fa_gh@protonmail.com>
2023-02-19 13:26:52 +0200

    Added a method for setting the radius boundary

--- Interfaces/IGeoLocation.cs
+++ Interfaces/IGeoLocation.cs
@@ -1,7 +1,10 @@
-﻿namespace smart_alert_api.Interfaces
+﻿using Geolocation;
+
+namespace smart_alert_api.Interfaces
 {
     public interface IGeoLocation
     {
         public double GetDistanceInKm(double latitude, double longitude, double otherLatitude, double otherLongitude);
+        public CoordinateBoundaries SetRadiusBoundaries(double latitude, double longitude, int radius);
     }
 }
