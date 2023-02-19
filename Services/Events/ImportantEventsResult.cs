8bfb41d3dbe6bfe4dde5d2d54ffb1e2e8e49436f
0x-2FA <2fa_gh@protonmail.com>
2023-02-19 13:46:46 +0200

    Changed from list to simple Event

--- Services/Events/ImportantEventsResult.cs
+++ Services/Events/ImportantEventsResult.cs
@@ -2,5 +2,5 @@
 
 namespace smart_alert_api.Services.Events
 {
-    public record ImportantEventsResult(List<Event>? earthquakeEvents, List<Event>? fireEvents, List<Event>? floodEvents);
+    public record ImportantEventsResult(Event? earthquakeEvents, Event? importantFireEvent, Event? importantFloodEvent);
 }
